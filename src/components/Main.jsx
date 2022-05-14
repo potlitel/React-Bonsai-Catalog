99b87fa50a94c3f4a7f93eba3db10f63a1f11e74
potlitel <potlitel@gmail.com>
2022-05-13 22:54:02 -0400

    Se incorporan mas ejemplos de datos en el fichero data.js

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -27,17 +27,14 @@ function Main({ details }) {
 
   function searchList() {
     return (
-      <Scroll>
+      <div>
         <DataList filteredPersons={filteredPersons} />
-      </Scroll>
+      </div>
     )
   }
 
   return (
     <section className="garamond">
-      <div className="navy georgia ma0 grow">
-        <h2 className="f2">Search your course</h2>
-      </div>
       <div className="pa2">
         <input 
           className="pa3 bb br3 grow b--none bg-lightest-blue ma3"
